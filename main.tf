8d2afb2f387ceb97c12cea2aac6ecfbf41bb8999
Greene <keith.w.greene@capgemini.com>
2021-01-22 10:17:02 -0500

    added main.tf

---
+++ main.tf
@@ -0,0 +1,38 @@
+provider "azurerm"{
+    version = "> 0.14"
+    features {}
+}
+
+resource "azurerm_resource_group" "tf_test" {
+    name = "tfmainrg"
+    location = "eastus"
+}
+
+data "azurerm_subscription" "current" {
+}
+
+output "current_subscription_display_name" {
+  value = data.azurerm_subscription.current.display_name
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+    name                    = "weatherapi"
+    location                = azurerm_resource_group.tf_test.location
+    resource_group_name     = azurerm_resource_group.tf_test.name
+
+    ip_address_type         = "public"
+    dns_name_label          = "officialmandalorianwa"
+    os_type                 = "Linux"
+
+    container {
+        name                = "weatherapi"
+            image           = "officialmandalorian/weatherapi"
+            cpu             = "1"
+            memory          = "1"
+
+            ports {
+                port        = 80
+                protocol    = "TCP"
+            }
+    }
+}
